08fb6b48869e61ede67b40b5f9600917b1b110f8
f1eeman <mr.f1eeman@gmail.com>
2026-02-02 18:46:00 +0500

    add npm scripts for development and production builds, enhance typescript configuration with strict settings and path aliases, refactor webpack config to use modular build system with environment-based configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,28 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
-import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import { buildWebpackConfig } from "./config/build/build-webpackConfig";
+import type { BuildEnv, BuildPaths } from "./config/build/types";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+export default (env: BuildEnv): webpack.Configuration => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
+  };
 
-export default {
-  mode: "development",
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  entry: "./src/index.ts",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
-  },
+  const mode = env.mode ?? "development";
+  const analyze = env.analyze ?? "disabled";
+  const PORT = env.port ?? 3000;
+
+  const isDev = mode === "development";
+  const isAnalyzeModeEnable = analyze === "enabled";
+
+  return buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+    isAnalyzeModeEnable,
+  });
 };

4e5f0bcf334fa017ae7c34c363ac18415d218824
f1eeman <mr.f1eeman@gmail.com>
2026-02-18 12:39:06 +0500

    refactor(storybook): replace useEffect with wrapper div in ThemeDecorator

--- src/shared/lib/sb/decorators/Theme.tsx
+++ src/shared/lib/sb/decorators/Theme.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import type { Decorator } from '@storybook/react'
 import type { Theme } from '@/app/theme'
 
@@ -6,14 +5,12 @@ type ThemeDecoratorType = (theme: Theme) => Decorator
 
 export const ThemeDecorator: ThemeDecoratorType = (theme: Theme) => {
   const DecoratorComponent: Decorator = (Story) => {
-    useEffect(() => {
-      const storybookRoot = document.querySelector('#storybook-root')
-      if (storybookRoot) {
-        storybookRoot.className = `app ${theme}`
-      }
-    }, [])
-
-    return <Story />
+    return (
+      <div className={`app-sb ${theme}`}>
+        <Story />
+      </div>
+    )
   }
+
   return DecoratorComponent
 }

152f111ef9d1d932d164339404a9ecf6d4d1bec0
f1eeman <mr.f1eeman@gmail.com>
2026-02-23 20:48:18 +0500

    feat(fonts): add Roboto font family with optimized asset loading
    - Add Roboto font files (400, 500, 700 weights) in woff2 format
    - Create fonts.scss with @font-face declarations for Roboto variants
    - Update global font family from Consolas to Roboto with fallbacks
    - Add font rendering optimization for consistent screenshot testing
    - Split webpack file loader into separate fonts and images loaders
    - Configure asset/resource type with custom output paths for fonts and images

--- config/build/build-loaders.ts
+++ config/build/build-loaders.ts
@@ -1,26 +1,36 @@
-import { buildCssLoader } from "./loaders/css-loader";
-import type webpack from "webpack";
-import type { BuildOptions } from "./types";
+import { buildCssLoader } from './loaders/css-loader'
+import type webpack from 'webpack'
+import type { BuildOptions } from './types'
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
-    use: ["@svgr/webpack"],
-  };
+    use: ['@svgr/webpack'],
+  }
 
-  const cssLoader = buildCssLoader(isDev);
-
+  const cssLoader = buildCssLoader(isDev)
 
   const tsLoader = {
     test: /\.tsx?$/,
-    use: "ts-loader",
+    use: 'ts-loader',
     exclude: /node_modules/,
-  };
+  }
 
-  const fileLoader = {
-    test: /\.(png|jpe?g|gif|woff2)$/i,
-    type: "asset",
-  };
+  const fontsLoader = {
+    test: /\.(woff2|woff)$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'assets/fonts/[name][ext]',
+    },
+  }
+
+  const imagesLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'assets/img/[name].[hash:8][ext]',
+    },
+  }
 
   // const babelLoader = {
   //   test: /\.(js|ts|jsx|tsx)$/,
@@ -33,5 +43,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   //   },
   // };
 
-  return [cssLoader, svgLoader, fileLoader, tsLoader];
+  return [cssLoader, svgLoader, tsLoader, fontsLoader, imagesLoader]
 }

da6d05cbf592304b8f836ebb075c182c96dc6350
f1eeman <mr.f1eeman@gmail.com>
2026-02-25 19:09:54 +0500

    refactor(store): memoize store creation in StoreProvider and optimize imports

--- src/app/store/Provider.tsx
+++ src/app/store/Provider.tsx
@@ -1,7 +1,7 @@
+import { useMemo, type FC, type ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { setupStore } from './store'
-import type { FC, ReactNode } from 'react'
-import type { RootState, AppStore } from './store'
+import type { RootState } from './store'
 import type { DeepPartial } from '@/shared/types'
 
 interface StoreProviderProps {
@@ -13,6 +13,7 @@ export const StoreProvider: FC<StoreProviderProps> = ({
   children,
   initialState,
 }) => {
-  const store: AppStore = setupStore(initialState)
+  const store = useMemo(() => setupStore(initialState), [initialState])
+
   return <Provider store={store}>{children}</Provider>
 }

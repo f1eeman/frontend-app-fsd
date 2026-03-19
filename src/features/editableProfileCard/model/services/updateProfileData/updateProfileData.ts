94b8dfec3788b662e9772ecfafabf4d6f20a0de7
f1eeman <mr.f1eeman@gmail.com>
2026-03-20 01:38:24 +0500

    feat(profile): add validation

--- src/features/editableProfileCard/model/services/updateProfileData/updateProfileData.ts
+++ src/features/editableProfileCard/model/services/updateProfileData/updateProfileData.ts
@@ -1,23 +1,35 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { selectProfileForm } from '../../slice/profileSlice'
+import { ValidateProfileError } from '../../types/profile'
+import { validateProfileData } from '../validateProfileData/validateProfileData'
 import type { Profile } from '../../types/profile'
 import type { ThunkConfig } from '@/app/store'
 
 export const updateProfileData = createAsyncThunk<
   Profile,
   void,
-  ThunkConfig<string>
+  ThunkConfig<ValidateProfileError[]>
 >('profile/updateProfileData', async (_, thunkApi) => {
   const { extra, rejectWithValue, getState } = thunkApi
 
   const formData = selectProfileForm(getState())
 
+  const errors = validateProfileData(formData)
+
+  if (errors.length) {
+    return rejectWithValue(errors)
+  }
+
   try {
     const response = await extra.api.put<Profile>('/profile', formData)
+
+    if (!response.data) {
+      throw new Error()
+    }
 
     return response.data
   } catch (e) {
     console.log(e)
-    return rejectWithValue('error')
+    return rejectWithValue([ValidateProfileError.SERVER_ERROR])
   }
 })
